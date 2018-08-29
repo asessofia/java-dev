2be9da4c744292835a8c1c4ffb35373014d6d207
Macvey <jmacvey1@LAMU02VV54HHTD6.uhc.com>
2018-08-28 19:39:42 -0500

    added initial algorithm for stack resiz

--- src/main/java/structures/Stack.java
+++ src/main/java/structures/Stack.java
@@ -6,28 +6,38 @@ public class Stack {
 	private static int DEFAULT_MAX_STACK_SIZE = 10;
 	private String[] data;
 	private int topLevel;
+	private int maxSize;
+
+	private void resize() {
+		// Implement Parts 1.2 & 2 of algorithm here.
+
+		// increase max size to twice size
+		this.maxSize *= 2;
+	}
 
 	public Stack() {
 		this.data = new String[DEFAULT_MAX_STACK_SIZE];
 		this.topLevel = 0;
+		this.maxSize = DEFAULT_MAX_STACK_SIZE;
 	}
 
 	public void pop() {
 		if (this.topLevel != 0) {
-			this.topLevel--; // equiv to this.topLevel = this.topLevel - 1
+			this.topLevel--;
 		}
 	}
-	
+
 	public void push(String item) {
-		// TODO: IMPLEMENT PUSH
+		if (maxSize == topLevel) {
+			this.resize();
+		}
 		this.data[topLevel] = item;
 		++this.topLevel;
 	}
-	
+
 	public String top() {
-		// TODO: IMPLEMENT TOP
-		return data[topLevel-1];// STUB
-		
+		return data[topLevel - 1];
+
 	}
 
 }
