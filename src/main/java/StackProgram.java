c32097a40229f096c4212e695e6b34a66fb2dc0a
Sessofia <asessofi@LAMU02TJ7PHGTFM.uhc.com>
2018-08-30 20:00:45 -0500

    assignment completed with algorithm

--- src/main/java/StackProgram.java
+++ src/main/java/StackProgram.java
@@ -1,29 +1,40 @@
 package main.java;
+
 import main.java.structures.Stack;
 
 public class StackProgram extends MiniProgram {
-	
+
 	private void output(String label, Object value) {
 		System.out.println(label + ": " + value);
 	}
-	
+
 	private void testStackEmpty(Stack s) {
 		output("Stack Empty: ", s.isEmpty());
 	}
-	
+
 	@Override
 	public void execute() {
 		System.out.println("EXECUTING STACK PROGRAM\n");
 		Stack stack = new Stack();
 		testStackEmpty(stack);
-		
+
 		// Step 1: push Strings "0" -> "10" onto the stack
+
+		for (Integer i = 0; i <= 10; i++) {
+			stack.push(i.toString());
+		}
+
 		// Step 2: Test if stack is empty
+		testStackEmpty(stack);
+
 		// Step 3: Print the stack in reverse.
-		
-		Integer i = 0;
-		String iString = i.toString();
-		
+		for (Integer i = 10; i >= 0; i--) {
+			output("Top of the Stack", stack.top());
+
+			stack.pop();
+
+		}
+
 	}
 
 }
